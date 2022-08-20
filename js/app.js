33e2a4d52a0a674d132fa77c5b5a0067f67907f5
indrajith76 <indrajithgoswami@gmail.com>
2022-08-20 18:30:02 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -21,24 +21,49 @@ document.getElementById('generate-pin').addEventListener('click',function(){
     displayPinField.value = pin;
 });
 
-document.getElementById('calculator').addEventListener('click', function(event){
-    const number = event.target.innerText;
-    const typedNumberField = document.getElementById('typed-numbers');
-    const previousTypedNumber = typedNumberField.value;
-    if(isNaN(number)){
-        if(number === 'C'){
-            typedNumberField.value = '';
+const buttons = document.getElementsByClassName('button');
+for (const btn of buttons) {
+    btn.addEventListener('click',function(event){
+        const number = event.target.innerText;
+        const typedNumberField = document.getElementById('typed-numbers');
+        const previousTypedNumber = typedNumberField.value;
+        if(isNaN(number)){
+            if(number === 'C'){
+                typedNumberField.value = '';
+            }
+            else{
+                typedNumberField.value = typedNumberField.value.slice(0, -1);
+                /* const digits = previousTypedNumber.split('');
+                digits.pop();
+                const remainingDigits = digits.join('');
+                typedNumberField.value = remainingDigits; */
+            }
         }
         else{
-            typedNumberField.value = typedNumberField.value.slice(0, -1);
-            /* const digits = previousTypedNumber.split('');
-            digits.pop();
-            const remainingDigits = digits.join('');
-            typedNumberField.value = remainingDigits; */
+            const newTypedNumber = previousTypedNumber + number; 
+            typedNumberField.value = newTypedNumber;
         }
+    })
+}
+
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+    if(typedNumber === currentPin){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+
     }
     else{
-        const newTypedNumber = previousTypedNumber + number; 
-        typedNumberField.value = newTypedNumber;
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+
     }
 })
