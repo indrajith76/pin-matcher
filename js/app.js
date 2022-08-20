4d86b9588d12e0d1f9028b293e8c0391b9c6954e
indrajith76 <indrajithgoswami@gmail.com>
2022-08-20 17:12:21 +0600

    Use event bubble to create calculator and clear

---
+++ js/app.js
@@ -0,0 +1,44 @@
+function getPin(){
+    const pin = genratePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+}
+
+function genratePin(){
+    const random = Math.round(Math.random()*10000)
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click',function(){
+    const pin = getPin();
+    // display pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+});
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else{
+            typedNumberField.value = typedNumberField.value.slice(0, -1);
+            /* const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits; */
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number; 
+        typedNumberField.value = newTypedNumber;
+    }
+})
